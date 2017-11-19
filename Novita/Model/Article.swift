9e605ba51ddf1213ff21f2e0ac0f5ebd587029c9
c8112002 <c8112002@gmail.com>
2017-11-20 00:13:25 +0900

    add APIClientService

---
+++ Novita/Model/Article.swift
@@ -0,0 +1,17 @@
+//
+//  Article.swift
+//  Novita
+//
+//  Created by Masahiko Sato on 2017/11/19.
+//  Copyright © 2017年 Masahiko Sato. All rights reserved.
+//
+
+import Foundation
+
+struct Article {
+    let title: String
+    
+    init(title: String) {
+        self.title = title
+    }
+}
---
+++ Novita/Service/APIClientService.swift
@@ -0,0 +1,47 @@
+//
+//  APIClientService.swift
+//  Novita
+//
+//  Created by Masahiko Sato on 2017/11/19.
+//  Copyright © 2017年 Masahiko Sato. All rights reserved.
+//
+
+import Foundation
+import APIKit
+import RxSwift
+
+protocol APIRequest: Request {}
+
+extension APIRequest {
+    var baseURL: URL {
+        return URL(string: "http://localhost:3000/v1")!
+    }
+}
+
+struct ArticlesRequest: APIRequest {
+    
+    typealias Response = [Article]
+    
+    var method: HTTPMethod {
+        return .get
+    }
+    
+    var path: String {
+        return "/articles"
+    }
+    
+    func response(from object: Any, urlResponse: HTTPURLResponse) throws -> [Article] {
+        return [Article(title: "")]
+    }
+}
+
+
+protocol APIClientServiceType: class {
+    func requestArticles() -> Observable<[Article]>
+}
+
+final class APIClientService: APIClientServiceType {
+    func requestArticles() -> Observable<[Article]> {
+        return Session.rx.send(request: ArticlesRequest())
+    }
+}
